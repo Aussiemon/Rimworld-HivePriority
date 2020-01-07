08db827f6efcb04e5738d126b43502f847d69c05
Aussiemon <mattrohrlach+github@gmail.com>
2020-01-06 21:31:03 -0700

    Initialized project with single xpath patch and license

---
+++ Source/Hive_Priority.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using Verse.AI;
+using System.Diagnostics;
+
+namespace Hive_Priority
+{
+    public class Hive_Priority
+    {
+    }
+
+    internal class Debug
+    {
+        [Conditional("DEBUG")]
+        internal static void Log(string s)
+        {
+            Verse.Log.Message(s);
+        }
+		
+        [Conditional("DEBUG")]
+        internal static void Warning(string s)
+        {
+            Verse.Log.Warning(s);
+        }
+
+        [Conditional("DEBUG")]
+        internal static void Error(string s)
+        {
+            Verse.Log.Error(s);
+        }
+    }
+}
